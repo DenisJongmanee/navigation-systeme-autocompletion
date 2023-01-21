ea7bc4ac7258f55ac5fb03470ad27c95e7f9ba2c
DenisJongmanee <jongmanee.denis@gmai.com>
2023-01-21 10:03:43 +0100

    integration google map api, affichage d'une carte, coordonnées fixe

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,58 @@
 import React from "react";
 import '../App.css';
+import { GoogleMap, LoadScript, MarkerF, DirectionsRenderer } from '@react-google-maps/api';
 
-function Home(){
-    
-    return(
+
+function Home() {
+    const mapContainerStyle = {
+        position: "absolute",
+        top: 0,
+        right: 0,
+        width:"80%",
+        height:"100%"
+    }
+
+    const center = {
+        lat: (48.8588897+50.6365654)/2,
+        lng: (2.320041+3.0635282)/2
+    }
+
+    const destination = {
+        lat: 48.8588897,
+        lng: 2.320041
+    }
+
+    const origin = {
+        lat: 50.6365654,
+        lng: 3.0635282
+    }
+
+    const onLoad = marker => {
+        console.log('marker: ', marker)
+    }
+
+    return (
         <div className="Home">
             <h1>Home Page</h1>
+            <LoadScript
+                googleMapsApiKey="AIzaSyCjzCI3yJUBRRNwH822WmWNcOzFeFf-qvk"
+            >
+                <GoogleMap
+                    id="marker-example"
+                    mapContainerStyle={mapContainerStyle}
+                    zoom={7}
+                    center={center}
+                >
+                    <MarkerF
+                        onLoad={onLoad}
+                        position={origin}
+                    />
+                    <MarkerF
+                        onLoad={onLoad}
+                        position={destination}
+                    />
+                </GoogleMap>
+            </LoadScript>
         </div>
     )
 }
